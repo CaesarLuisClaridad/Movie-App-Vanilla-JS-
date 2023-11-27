d7fd545bf6fcedbb9194943da1ab692ea930886f
Caesar Luis II M. Claridad <sesar091913@gmail.com>
2023-11-27 10:11:10 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.querySelector("main");
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+
+//get movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+  const response = await fetch(url);
+  const respData = await response.json();
+
+  showMovies(respData.results);
+}
+
+//show all best movies
+function showMovies(movies) {
+  //clear main
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+            <img src="${IMGPATH + poster_path}" alt="${title}">
+    
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${colorAverage(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                ${overview}
+            </div>
+        `;
+    main.appendChild(movieEl);
+  });
+}
+
+//color gor vote_average
+function colorAverage(vote_average) {
+  if (vote_average >= 8) {
+    return "green";
+  }
+  if (vote_average >= 5) {
+    return "orange";
+  }
+  if (vote_average <= 4) {
+    return "red";
+  }
+}
+
+
+//search movies
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const search_movies = input.value
+
+    if(search_movies.length) {
+        getMovies(SEARCHAPI + search_movies)
+
+        input.value = ''
+    }
+})
+
+
